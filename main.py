115ea9d95ae84d987cadc5aeb90b623a32d34b48
Per Mortensen <per@permortensen.com>
2018-04-14 10:32:31 +0200

    Format with Black

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sevenseg
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     text = "Hello sevenseg"
     sevenseg.display(text, max_loops=2)
--- sevenseg.py
+++ sevenseg.py
@@ -1,18 +1,18 @@
-'''Module for displaying arbitrary strings on a 7-segment display.'''
+"""Module for displaying arbitrary strings on a 7-segment display."""
 
 import RPi.GPIO as GPIO
 import time
 
-'''GPIO ports for the 7 segment pins'''
-segments = (11,4,23,8,7,10,18,25)
+"""GPIO ports for the 7 segment pins"""
+segments = (11, 4, 23, 8, 7, 10, 18, 25)
 
-'''GPIO ports for the digit 0-3 pins'''
-digits = (22,27,17,24)
+"""GPIO ports for the digit 0-3 pins"""
+digits = (22, 27, 17, 24)
 
-'''Width of the display in characters'''
+"""Width of the display in characters"""
 width = len(digits)
 
-'''
+"""
 Patterns of on/off segments in the following order:
    __1__
   |     |
@@ -21,54 +21,54 @@ Patterns of on/off segments in the following order:
   |     |
 5 |     | 3
   |__4__|    . 8
-'''
+"""
 patterns = {
-    ' ': (0,0,0,0,0,0,0,0),
-    '0': (1,1,1,1,1,1,0,0),
-    '1': (0,1,1,0,0,0,0,0),
-    '2': (1,1,0,1,1,0,1,0),
-    '3': (1,1,1,1,0,0,1,0),
-    '4': (0,1,1,0,0,1,1,0),
-    '5': (1,0,1,1,0,1,1,0),
-    '6': (1,0,1,1,1,1,1,0),
-    '7': (1,1,1,0,0,0,0,0),
-    '8': (1,1,1,1,1,1,1,0),
-    '9': (1,1,1,1,0,1,1,0),
-    'A': (1,1,1,0,1,1,1,0),
-    'B': (0,0,1,1,1,1,1,0),
-    'C': (1,0,0,1,1,1,0,0),
-    'D': (0,1,1,1,1,0,1,0),
-    'E': (1,0,0,1,1,1,1,0),
-    'F': (1,0,0,0,1,1,1,0),
-    'G': (1,0,1,1,1,1,0,0),
-    'H': (0,0,1,0,1,1,1,0),
-    'I': (0,1,1,0,0,0,0,0),
-    'J': (0,1,1,1,0,0,0,0),
-    'K': (0,1,1,0,1,1,1,0),
-    'L': (0,0,0,1,1,1,0,0),
-    'M': (1,1,1,0,1,1,0,0),
-    'N': (0,0,1,0,1,0,1,0),
-    'O': (0,0,1,1,1,0,1,0),
-    'P': (1,1,0,0,1,1,1,0),
-    'Q': (1,1,1,0,0,1,1,0),
-    'R': (0,0,0,0,1,0,1,0),
-    'S': (1,0,1,1,0,1,1,0),
-    'T': (0,0,0,1,1,1,1,0),
-    'U': (0,1,1,1,1,1,0,0),
-    'V': (0,0,1,1,1,0,0,0),
-    'W': (0,0,1,1,1,0,0,0),
-    'X': (0,1,1,0,1,1,1,0),
-    'Y': (0,1,1,1,0,1,1,0),
-    'Z': (1,1,0,1,1,0,1,0),
-    '-': (0,0,0,0,0,0,1,0),
-    '_': (0,0,0,1,0,0,0,0),
-    '.': (0,0,0,0,0,0,0,1),
-    ',': (0,0,0,0,0,0,0,1)
+    " ": (0, 0, 0, 0, 0, 0, 0, 0),
+    "0": (1, 1, 1, 1, 1, 1, 0, 0),
+    "1": (0, 1, 1, 0, 0, 0, 0, 0),
+    "2": (1, 1, 0, 1, 1, 0, 1, 0),
+    "3": (1, 1, 1, 1, 0, 0, 1, 0),
+    "4": (0, 1, 1, 0, 0, 1, 1, 0),
+    "5": (1, 0, 1, 1, 0, 1, 1, 0),
+    "6": (1, 0, 1, 1, 1, 1, 1, 0),
+    "7": (1, 1, 1, 0, 0, 0, 0, 0),
+    "8": (1, 1, 1, 1, 1, 1, 1, 0),
+    "9": (1, 1, 1, 1, 0, 1, 1, 0),
+    "A": (1, 1, 1, 0, 1, 1, 1, 0),
+    "B": (0, 0, 1, 1, 1, 1, 1, 0),
+    "C": (1, 0, 0, 1, 1, 1, 0, 0),
+    "D": (0, 1, 1, 1, 1, 0, 1, 0),
+    "E": (1, 0, 0, 1, 1, 1, 1, 0),
+    "F": (1, 0, 0, 0, 1, 1, 1, 0),
+    "G": (1, 0, 1, 1, 1, 1, 0, 0),
+    "H": (0, 0, 1, 0, 1, 1, 1, 0),
+    "I": (0, 1, 1, 0, 0, 0, 0, 0),
+    "J": (0, 1, 1, 1, 0, 0, 0, 0),
+    "K": (0, 1, 1, 0, 1, 1, 1, 0),
+    "L": (0, 0, 0, 1, 1, 1, 0, 0),
+    "M": (1, 1, 1, 0, 1, 1, 0, 0),
+    "N": (0, 0, 1, 0, 1, 0, 1, 0),
+    "O": (0, 0, 1, 1, 1, 0, 1, 0),
+    "P": (1, 1, 0, 0, 1, 1, 1, 0),
+    "Q": (1, 1, 1, 0, 0, 1, 1, 0),
+    "R": (0, 0, 0, 0, 1, 0, 1, 0),
+    "S": (1, 0, 1, 1, 0, 1, 1, 0),
+    "T": (0, 0, 0, 1, 1, 1, 1, 0),
+    "U": (0, 1, 1, 1, 1, 1, 0, 0),
+    "V": (0, 0, 1, 1, 1, 0, 0, 0),
+    "W": (0, 0, 1, 1, 1, 0, 0, 0),
+    "X": (0, 1, 1, 0, 1, 1, 1, 0),
+    "Y": (0, 1, 1, 1, 0, 1, 1, 0),
+    "Z": (1, 1, 0, 1, 1, 0, 1, 0),
+    "-": (0, 0, 0, 0, 0, 0, 1, 0),
+    "_": (0, 0, 0, 1, 0, 0, 0, 0),
+    ".": (0, 0, 0, 0, 0, 0, 0, 1),
+    ",": (0, 0, 0, 0, 0, 0, 0, 1),
 }
 
 
 def display(text, max_loops=0):
-    '''Display a string, looping it forever or a specified number of times.'''
+    """Display a string, looping it forever or a specified number of times."""
     offset = 0
     loop_count = 0
     text = _clean(text)
@@ -101,7 +101,7 @@ def display(text, max_loops=0):
 
 
 def _reset():
-    '''Reset the display.'''
+    """Reset the display."""
     GPIO.setmode(GPIO.BCM)
 
     for segment in segments:
@@ -114,7 +114,7 @@ def _reset():
 
 
 def _render(s):
-    '''Render a display-width string.'''
+    """Render a display-width string."""
     for digit in range(width):
         for loop in range(0, 8):
             char = s[digit].upper()
@@ -127,5 +127,5 @@ def _render(s):
 
 
 def _clean(s):
-    '''Remove unsupported characters and add leading space for ticker-like behavior.'''
-    return ''.rjust(width) + ''.join(c for c in s.upper() if c in patterns)
+    """Remove unsupported characters and add leading space for ticker-like behavior."""
+    return "".rjust(width) + "".join(c for c in s.upper() if c in patterns)
